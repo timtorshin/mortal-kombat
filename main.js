31df9bdea126f59199b3454480cb428b25bbfc9e
Timofei Torshin <timtorshin@gmail.com>
2022-03-01 16:37:22 +0300

    refactor: added classes

--- main.js
+++ main.js
@@ -1,35 +1,5 @@
-import { $arenas, $formFight } from './constants/constants.js';
-import { firstPlayer, secondPlayer } from './constants/players.js';
-import { generateLogs } from './utils/logs.js';
-import { createPlayer } from './utils/creations.js';
-import { enemyAttack, playerAttack, fightResult } from './utils/actions.js';
+import Game from './components/Game.js';
 
-$arenas.appendChild(createPlayer(firstPlayer));
-$arenas.appendChild(createPlayer(secondPlayer));
+const game = new Game();
 
-generateLogs('start', firstPlayer, secondPlayer);
-
-$formFight.addEventListener('submit', function(evt) {
-  evt.preventDefault();
-
-  const enemy = enemyAttack();
-  const player = playerAttack();
-
-  if (enemy.hit !== player.defence) {
-    firstPlayer.changeHP(enemy.value);
-    firstPlayer.renderHP();
-    generateLogs('hit', secondPlayer, firstPlayer, enemy.value);
-  } else {
-    generateLogs('defence', firstPlayer, secondPlayer)
-  }
-
-  if (player.hit !== enemy.defence) {
-    secondPlayer.changeHP(player.value);
-    secondPlayer.renderHP();
-    generateLogs('hit', firstPlayer, secondPlayer, player.value);
-  } else {
-    generateLogs('defence', secondPlayer, firstPlayer)
-  }
-
-  fightResult();
-});
+game.start();
--- utils/utils.js
+++ utils/utils.js
@@ -7,3 +7,13 @@ export const getCurrentTime = () => {
 export const getRandom = (number) => {
   return Math.ceil(Math.random() * number);
 };
+
+export const createElement = (tag, className) => {
+  const $tag = document.createElement(tag);
+
+  if (className) {
+    $tag.classList.add(className);
+  }
+
+  return $tag;
+};
